10eb46a2c5314e3e23c1cb3415cdfeb24e15bd3d
Loic Dachary <loic@dachary.org>
2011-03-22 11:44:50 -0400

    implement participate, player2game, pick

--- cardstories/service.py
+++ cardstories/service.py
@@ -17,6 +17,7 @@
 # "AGPLv3".  If not, see <http://www.gnu.org/licenses/>.
 #
 import os
+import random
 
 from twisted.python import log
 from twisted.application import service
@@ -26,10 +27,17 @@ from twisted.enterprise import adbapi
 
 from OpenSSL import SSL
 
+NCARDS = 36
+NPLAYERS = 6
+CARDS_PER_PLAYER = 7
+
 class CardstoriesService(service.Service):
 
     def __init__(self, settings):
         self.settings = settings
+        self.NCARDS = NCARDS
+        self.NPLAYERS = NPLAYERS
+        self.CARDS_PER_PLAYER = CARDS_PER_PLAYER
 
     def startService(self):
         database = self.settings['db']
@@ -41,9 +49,12 @@ class CardstoriesService(service.Service):
             c.execute(
                 "CREATE TABLE games ( " +
                 "  id INTEGER PRIMARY KEY, " +
+                "  owner_id INTEGER, " +
+                "  players INTEGER DEFAULT 1, " +
                 "  sentence TEXT, " +
-                "  cards VARCHAR(36), " +
-                "  state VARCHAR(8) DEFAULT 'picking', " +
+                "  cards VARCHAR(%d), " % self.NCARDS +
+                "  board VARCHAR(%d), " % self.NPLAYERS +
+                "  state VARCHAR(8) DEFAULT 'invitation', " + # invitation, vote
                 "  created DATETIME, " +
                 "  completed DATETIME" +
                 "); ")
@@ -63,11 +74,13 @@ class CardstoriesService(service.Service):
                 "CREATE TABLE player2game ( " +
                 "  player_id INTEGER, " +
                 "  game_id INTEGER, " +
-                "  cards VARCHAR(7), " +
-                "  win BOOL " +
+                "  cards VARCHAR(%d), " % self.CARDS_PER_PLAYER +
+                "  picked CHAR(1), " +
+                "  vote CHAR(1), " +
+                "  win CHAR(1) DEFAULT 'n' " +
                 "); ")
             c.execute(
-                "CREATE INDEX player2game_idx ON player2game (player_id, game_id); "
+                "CREATE UNIQUE INDEX player2game_idx ON player2game (player_id, game_id); "
                 )
             db.commit()
             db.close()
@@ -79,10 +92,76 @@ class CardstoriesService(service.Service):
     def stopService(self):
         return defer.succeed(None)
 
-    def create(self):
-        d = self.db.runOperation("INSERT INTO games (created) VALUES (date('now'))")
-        d.addCallback(lambda result: {})
+    def createInteraction(self, transaction, card, sentence, owner_id):
+        cards = [ chr(x) for x in range(1, self.NCARDS + 1) ]
+        cards.remove(card)
+        random.shuffle(cards)
+        transaction.execute("INSERT INTO games (sentence, cards, board, owner_id, created) VALUES (?, ?, ?, ?, date('now'))", [sentence, ''.join(cards), card, owner_id])
+        game_id = transaction.lastrowid
+        transaction.execute("INSERT INTO player2game (game_id, player_id, cards, picked) VALUES (?, ?, ?, ?)", [ game_id, owner_id, card, card ])
+        return game_id
+
+    def create(self, args):
+        card = chr(int(args['card'][0]))
+        sentence = args['sentence'][0]
+        owner_id = int(args['owner_id'][0])
+        d = self.db.runInteraction(self.createInteraction, card, sentence, owner_id)
+        d.addCallback(lambda game_id: {'game_id': game_id})
         return d
+
+    def creationKit(self):
+        d = defer.succeed(True)
+        d.addCallback(lambda result: { 'cards': range(1, self.NCARDS + 1) })
+        return d
+
+    def participateInteraction(self, transaction, game_id, player_id):
+        try:
+            transaction.execute("SELECT players, cards FROM games WHERE id = %d" % game_id)
+            ( players, cards ) = transaction.fetchall()[0]
+            no_room = Exception('player %d cannot join game %d because the %d players limit is reached' % ( player_id, game_id, self.NPLAYERS ))
+            if players >= self.NPLAYERS:
+                raise no_room
+            transaction.execute("UPDATE games SET cards = ?, players = players + 1 WHERE id = %d AND players = %d" % ( game_id, players ), [ cards[self.CARDS_PER_PLAYER:] ])
+            if transaction.rowcount == 0:
+                raise no_room
+            transaction.execute("INSERT INTO player2game (game_id, player_id, cards) VALUES (?, ?, ?)", [ game_id, player_id, cards[:self.CARDS_PER_PLAYER] ])
+            return {}
+        except Exception, e:
+            return {'error': e.args[0]}
+
+    def participate(self, args):
+        player_id = int(args['player_id'][0])
+        game_id = int(args['game_id'][0])
+        return self.db.runInteraction(self.participateInteraction, game_id, player_id)
+
+    @defer.inlineCallbacks
+    def voting(self, args):
+        game_id = int(args['game_id'][0])
+        owner_id = int(args['owner_id'][0])
+        rows = yield self.db.runQuery("SELECT picked FROM player2game WHERE game_id = %d AND picked IS NOT NULL")
+        board = ''.join(map(lambda row: row[0], rows))
+        yield self.db.runOperation("UPDATE games SET board = ?, state = 'voting' WHERE id = %d AND owner_id = %d" % ( game_id, owner_id ), [ board ])
+
+    @defer.inlineCallbacks
+    def player2game(self, args):
+        player_id = int(args['player_id'][0])
+        game_id = int(args['game_id'][0])
+        rows = yield self.db.runQuery("SELECT cards, picked, vote, win FROM player2game WHERE game_id = %d AND player_id = %d" % ( game_id, player_id ))
+        def c(c):
+            if c:
+                return ord(c)
+            else:
+                return c
+        defer.returnValue({ 'cards': map(lambda c: ord(c), rows[0][0]),
+                            'picked': c(rows[0][2]),
+                            'vote': c(rows[0][2]),
+                            'win': rows[0][3] })
+
+    def pick(self, args):
+        player_id = int(args['player_id'][0])
+        game_id = int(args['game_id'][0])
+        card = int(args['card'][0])
+        return self.db.runOperation("UPDATE player2game SET picked = ? WHERE game_id = %d AND player_id = %d" % ( game_id, player_id ), [ chr(card) ])
 
     def tick(self):
         return defer.succeed(True)
--- tests/test-service.py
+++ tests/test-service.py
@@ -66,147 +66,103 @@ class CardstoriesServiceTestRun(unittest.TestCase):
         d.addCallback(lambda result: self.assertTrue(result))
         return d
 
-class CardstoriesServiceTestCreateGet(CardstoriesServiceTest):
+class CardstoriesServiceTest(CardstoriesServiceTest):
 
     def test01_create(self):
-        d = self.service.create()
+        card = 5
+        sentence = 'SENTENCE'
+        owner_id = 15
+        d = self.service.create({ 'card': [card],
+                                  'sentence': [sentence],
+                                  'owner_id': [owner_id]})
         def check(result):
             c = self.db.cursor()
             c.execute("SELECT * FROM games")
             rows = c.fetchall()
             self.assertEquals(1, len(rows))
+            self.assertEquals(result['game_id'], rows[0][0])
+            self.assertEquals(owner_id, rows[0][1])
+            one_player = 1
+            self.assertEquals(one_player, rows[0][2])
+            self.assertEquals(sentence, rows[0][3])
+            self.assertFalse(chr(card) in rows[0][4])
+            self.assertEquals(chr(card), rows[0][5])
+            c.execute("SELECT cards FROM player2game WHERE game_id = %d AND player_id = %d" %  ( result['game_id'], owner_id ))
+            rows = c.fetchall()
+            self.assertEquals(1, len(rows))
+            self.assertEquals(chr(card), rows[0][0])
             c.close()
         d.addCallback(check)
         return d
 
-#    @defer.inlineCallbacks
-#    def test02_get(self):
-#        card1 = 'CARD1'
-#        yield self.service.create({ 'card': [card1]})
-#        card2 = 'CARD2'
-#        yield self.service.create({ 'card': [card2]})
-#        yield self.service.db.runOperation("UPDATE cards SET alive = datetime('now') WHERE card = '%s'" % card1)
-#        yield self.service.db.runOperation("UPDATE cards SET alive = datetime('now','+1 hour') WHERE card = '%s'" % card2)
-#        rows = yield self.service.get()
-#        self.assertEquals(card2, rows['rows'][0][1])
-#        self.assertEquals(card1, rows['rows'][1][1])
-
-class CardstoriesServiceTestPing(CardstoriesServiceTest):
-
-    def setUp(self):
-        CardstoriesServiceTest.setUp(self)
-
-        self.url_behaviour = 'OK'
-
-        class R(resource.Resource):
-            isLeaf = True
-            def render_GET(Rself, request):
-                if self.url_behaviour == 'OK':
-                    if request.path == '/':
-                        return 'CONTENT'
-                    else:
-                        request.setResponseCode(http.NOT_FOUND)
-                        return 'NOT OK 404'
-                elif self.url_behaviour == '500':
-                    request.setResponseCode(http.INTERNAL_SERVER_ERROR)
-                    return 'NOT OK 500'
-                elif self.url_behaviour == 'timeout':
-                    return server.NOT_DONE_YET
-
-        self.server = reactor.listenTCP(0, server.Site(R()), interface='127.0.0.1')
-        self.port = self.server.getHost().port
-
-    def tearDown(self):
-        CardstoriesServiceTest.tearDown(self)
-        return self.server.stopListening()
+    @defer.inlineCallbacks
+    def test02_participate(self):
+        card = 5
+        sentence = 'SENTENCE'
+        owner_id = 15
+        game = yield self.service.create({ 'card': [card],
+                                           'sentence': [sentence],
+                                           'owner_id': [owner_id]})
+        c = self.db.cursor()
+        c.execute("SELECT LENGTH(cards) FROM games WHERE id = %d" % game['game_id'])
+        cards_length = c.fetchone()[0]
+        player_id = 23
+        participation = yield self.service.participate({ 'player_id': [player_id],
+                                                         'game_id': [game['game_id']] })
+        self.assertEquals({}, participation)
+        c.execute("SELECT LENGTH(cards) FROM games WHERE id = %d" % game['game_id'])
+        self.assertEquals(cards_length - self.service.CARDS_PER_PLAYER, c.fetchone()[0])
+        c.execute("SELECT LENGTH(cards) FROM player2game WHERE game_id = %d AND player_id = %d" % ( game['game_id'], player_id ))
+        self.assertEquals(self.service.CARDS_PER_PLAYER, c.fetchone()[0])
+        c.close()
 
     @defer.inlineCallbacks
-    def test00_ping_ok(self):
-        url = 'http://127.0.0.1:%d' % self.port
-        yield self.service.submit({ 'url': [url]})
-        rows = yield self.service.get()
-        self.assertEquals('down', rows['rows'][0][2])
-        self.assertEquals(None, rows['rows'][0][3]) # alive
-        self.assertEquals(None, rows['rows'][0][4]) # checked
-        # URL responds
-        self.url_behaviour = 'OK'
-        content = yield self.service.ping(1, url)
-        self.assertEquals('OK 1', content)
-        rows = yield self.service.get()
-        self.assertEquals('up', rows['rows'][0][2])
-        self.assertEquals(rows['rows'][0][3], rows['rows'][0][4]) # alive == checked
+    def test03_player2game(self):
+        card = 5
+        sentence = 'SENTENCE'
+        owner_id = 15
+        game = yield self.service.create({ 'card': [card],
+                                           'sentence': [sentence],
+                                           'owner_id': [owner_id]})
+        player_id = 23
+        yield self.service.participate({ 'player_id': [player_id],
+                                         'game_id': [game['game_id']] })
+        player = yield self.service.player2game({ 'player_id': [player_id],
+                                                  'game_id': [game['game_id']] })
+        self.assertEquals(self.service.CARDS_PER_PLAYER, len(player['cards']))
+        self.assertEquals(None, player['vote'])
+        self.assertEquals(u'n', player['win'])
+
+    @defer.inlineCallbacks
+    def test03_pick(self):
+        card = 5
+        sentence = 'SENTENCE'
+        owner_id = 15
+        game = yield self.service.create({ 'card': [card],
+                                           'sentence': [sentence],
+                                           'owner_id': [owner_id]})
+        player2card = {}
+        for player_id in ( 16, 17 ):
+            yield self.service.participate({ 'player_id': [player_id],
+                                             'game_id': [game['game_id']] })
+            player = yield self.service.player2game({ 'player_id': [player_id],
+                                                      'game_id': [game['game_id']] })
+            player2card[player_id] = player['cards'][0]
+            yield self.service.pick({ 'player_id': [player_id],
+                                      'game_id': [game['game_id']],
+                                      'card': [ player2card[player_id] ] })
         
-    @defer.inlineCallbacks
-    def test01_ping_500(self):
-        url = 'http://127.0.0.1:%d' % self.port
-        yield self.service.submit({ 'url': [url]})
-        yield self.service.db.runOperation("UPDATE urls SET checked = datetime('now','-1 hour'), alive = datetime('now','-1 hour'), state = 'up'")
-        rows = yield self.service.get()
-        self.assertEquals('up', rows['rows'][0][2])
-        self.assertEquals(rows['rows'][0][3], rows['rows'][0][4]) # alive == checked
-        rows = yield self.service.db.runQuery("SELECT * FROM urls WHERE checked > alive")
-        self.assertEquals([], rows)
-        # URL fails with HTTP error 500
-        self.url_behaviour = '500'
-        d = self.service.ping(1, url)
-        def error500(reason):
-            self.assertEquals('500', reason.value.status)
-            return True
-        d.addErrback(error500)
-        yield d
-        rows = yield self.service.get()
-        self.assertEquals('down', rows['rows'][0][2])
-        rows = yield self.service.db.runQuery("SELECT * FROM urls WHERE checked > alive")
-        self.assertEquals('down', rows[0][2])
-
-    @defer.inlineCallbacks
-    def test02_ping_timeout(self):
-        url = 'http://127.0.0.1:%d' % self.port
-        yield self.service.submit({ 'url': [url]})
-        yield self.service.db.runOperation("UPDATE urls SET state = 'up'")
-        # URL fails because the URL timesout
-        self.service.settings['ping-timeout'] = 0.1
-        self.url_behaviour = 'timeout'
-        d = self.service.ping(1, url)
-        def timeout(reason):
-            self.assertTrue(isinstance(reason.value, defer.TimeoutError))
-            return True
-        d.addErrback(timeout)
-        yield d
-        rows = yield self.service.get()
-        self.assertEquals('down', rows['rows'][0][2])
-
-    @defer.inlineCallbacks
-    def test03_pings(self):
-        parallel_pings = 2
-        self.service.settings['parallel-pings'] = parallel_pings
-        url_good = 'http://127.0.0.1:%d' % self.port
-        yield self.service.submit({ 'url': [url_good]})
-        url_bad = 'http://127.0.0.1:%d/not_found' % self.port
-        for i in ('1','2','3'):
-            yield self.service.submit({ 'url': [url_bad + i]})
-        # pass 1
-        results = yield self.service.pings()
-        self.assertEquals(parallel_pings, len(results))
-        self.assertEquals('OK 1', results[0])
-        self.assertEquals((2, str(http.NOT_FOUND)), (results[1][0], results[1][1].value.status))
-        # pass 2
-        results = yield self.service.pings()
-        self.assertEquals(parallel_pings, len(results))
-        self.assertEquals((3, str(http.NOT_FOUND)), (results[0][0], results[0][1].value.status))
-        self.assertEquals((4, str(http.NOT_FOUND)), (results[1][0], results[1][1].value.status))
-        # pass 3
-        results = yield self.service.pings()
-        self.assertEquals(parallel_pings, len(results))
-        self.assertEquals('OK 1', results[0])
-        self.assertEquals((2, str(http.NOT_FOUND)), (results[1][0], results[1][1].value.status))
-
+        c = self.db.cursor()
+        for player_id in ( 16, 17 ):
+            c.execute("SELECT picked FROM player2game WHERE game_id = %d AND player_id = %d" % ( game['game_id'], player_id ))
+            self.assertEquals(player2card[player_id], ord(c.fetchone()[0]))
+            
 def Run():
     loader = runner.TestLoader()
 #    loader.methodPrefix = "test_trynow"
     suite = loader.suiteFactory()
     suite.addTest(loader.loadClass(CardstoriesServiceTestInit))
-    suite.addTest(loader.loadClass(CardstoriesServiceTestCreateGet))
+    suite.addTest(loader.loadClass(CardstoriesServiceTest))
     suite.addTest(loader.loadClass(CardstoriesServiceTestRun))
 
     return runner.TrialRunner(
