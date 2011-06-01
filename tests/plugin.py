895c92864e74ae4dfc97f4e79a50c5b88aacede2
Loic Dachary <loic@dachary.org>
2011-06-01 16:58:11 +0200

    self document the accept method as an example in tests/plugin.py

--- tests/plugin.py
+++ tests/plugin.py
@@ -27,6 +27,40 @@ class Plugin:
 
     def accept(self, event):
         self.service.plugin_event = event
+        if event == None:
+            pass
+        # startService() was called on CardstoriesService
+        elif event['type'] == 'start': 
+            pass
+        # stopService() is about to be called on CardstoriesService
+        elif event['type'] == 'stop': 
+            pass
+        # a game is about to be deleted from memory
+        elif event['type'] == 'delete': 
+            event['game'] # CardstoriesGame instance
+        # a game was modified
+        elif event['type'] == 'change': 
+            event['game'] # CardstoriesGame instance
+            # a player is given cards to pick
+            if event['details']['type'] == 'participate':
+                event['details']['player_id'] # numerical player id
+            # the author went to voting state
+            elif event['details']['type'] == 'voting':
+                pass
+            # the player picks a card
+            elif event['details']['type'] == 'pick':
+                event['details']['player_id'] # numerical player id
+                event['details']['card'] # numerical card id
+            # the player votes for a card
+            elif event['details']['type'] == 'vote':
+                event['details']['player_id'] # numerical player id
+                event['details']['vote'] # numerical card id
+            # the author went to game complete state
+            elif event['details']['type'] == 'complete':
+                pass
+            # the author invited players to play
+            elif event['details']['type'] == 'invite':
+                event['details']['invited'] # list of numerical player ids
         self.service.listen().addCallback(self.accept)
 
     def preprocess(self, result, request):
