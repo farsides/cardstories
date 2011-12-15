2a2faaf1b6ecd794095941d99327f0a758a59aee
Matjaz Gregoric <mtyaka@gmail.com>
2011-12-15 22:30:46 +0100

    Remove trailing whitespace.

--- cardstories/game.py
+++ cardstories/game.py
@@ -181,13 +181,13 @@ class CardstoriesGame(pollable):
         for player in rows:
             # player_id
             players_id_list.append(player[0])
-            
+
             # player_cards
             if player[0] == player_id or owner_id == player_id:
                 player_cards = [ ord(c) for c in player[1] ]
             else:
                 player_cards = None
-                
+
             # picked
             if player[2] != None:
                 if (state == 'complete' or player[0] == player_id or owner_id == player_id):
@@ -198,11 +198,11 @@ class CardstoriesGame(pollable):
                 picked = None
             if player[2] != None:
                 picked_count += 1
-                
+
             # self
             if player[0] == player_id:
                 myself = [ self.ord(player[2]), self.ord(player[3]), player_cards ]
-                
+
             # vote / winner_card
             if state == 'complete' or owner_id == player_id:
                 if player[0] == owner_id:
@@ -215,19 +215,19 @@ class CardstoriesGame(pollable):
                     vote = None
             if player[3] != None:
                 voted_count += 1
-                
+
             # win
             win = player[4]
-                
+
             # players
             players.append({ 'id': player[0], 'cards': player_cards, 'picked': picked, 'vote': vote, 'win': win })
-            
+
         ready = None
         if state == 'invitation':
             ready = picked_count >= self.MIN_PICKED
         elif state == 'vote':
             ready = voted_count >= self.MIN_VOTED
-        defer.returnValue([{ 'id': self.get_id(),
+        defer.returnValue([{'id': self.get_id(),
                             'modified': self.get_modified(),
                             'sentence': sentence,
                             'winner_card': winner_card,
@@ -380,7 +380,7 @@ class CardstoriesGame(pollable):
         else:
             winners = failed + guessed
         transaction.execute("UPDATE player2game SET win = 'y' WHERE "
-                            "  game_id = %d AND " % game_id + 
+                            "  game_id = %d AND " % game_id +
                             "  player_id IN ( %s ) " % ','.join([ str(id) for id in winners ]))
         transaction.execute("UPDATE games SET completed = datetime('now'), state = 'complete' WHERE id = %d" % game_id)
 
@@ -412,5 +412,3 @@ class CardstoriesGame(pollable):
             return ord(c)
         else:
             return c
-
-
