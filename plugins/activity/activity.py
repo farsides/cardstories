345afc9e6d38a82000561a12341745d3db935878
Xavier Antoviaque <xavier@antoviaque.org>
2012-01-26 15:35:39 +0100

    Fixes #755 - Implement monitoring of player online status
    Implemented as a plugin, which monitors the active polls on the chat
    plugin (which is currently always active when the player is logged in).
    
    The activity plugin allows to request a list of online players using a
    state request of type 'activity' (can be used later on to implement the
    list of online players in the client), or other plugins through the
    Observable interface, by sending notification each time a player logs in
    or logs out.
    
    Players who drop polls are given time to reastablish one, to avoid false
    positives when a poll naturally ends (timeout or notification), or when
    a player refreshes the page or switch to a new game.

---
+++ plugins/activity/activity.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Farsides <contact@farsides.com>
+#
+# Authors:
+#          Xavier Antoviaque <xavier@antoviaque.org>
+#
+# This software's license gives you freedom; you can copy, convey,
+# propagate, redistribute and/or modify this program under the terms of
+# the GNU Affero General Public License (AGPL) as published by the Free
+# Software Foundation (FSF), either version 3 of the License, or (at your
+# option) any later version of the AGPL published by the FSF.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in a file in the toplevel directory called
+# "AGPLv3".  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports ########################################################################
+
+import os
+from twisted.internet import defer, reactor
+
+from cardstories.poll import Pollable
+from cardstories.helpers import Observable
+
+
+# Classes ########################################################################
+
+class Plugin(Pollable, Observable):
+    """
+    Monitors the activity of players (online/offline)
+    
+    """
+    def __init__(self, service, plugins):
+        self.service = service
+        self.observers = []
+        self.online_players = {}
+
+        # Depends on the chat plugin (monitors active polls on the chat)
+        for plugin in plugins:
+            if plugin.name() == 'chat':
+                self.chat_plugin = plugin
+        assert self.chat_plugin
+
+        self.chat_plugin.listen().addCallback(self.on_chat_notification)
+
+        # Implement the path conventions
+        self.confdir = os.path.join(self.service.settings['plugins-confdir'], self.name())
+        self.libdir = os.path.join(self.service.settings['plugins-libdir'], self.name())
+
+        # Initialize the pollable using the recommended timeout.
+        Pollable.__init__(self, self.service.settings.get('poll-timeout', 30))
+
+    def name(self):
+        """
+        Method required by all plugins to inspect the plugin's name.
+        
+        """
+        return 'activity'
+
+    def on_chat_notification(self, changes):
+        """
+        Listen to chat notifications to know when polls are started/ended
+        """
+
+        d = defer.succeed(True)
+
+        if changes != None and changes['type'] == 'poll_start':
+            self.on_chat_poll_start(int(changes['player_id']))
+
+        if changes != None and changes['type'] == 'poll_end':
+            self.on_chat_poll_end(int(changes['player_id']))
+
+        self.chat_plugin.listen().addCallback(self.on_chat_notification)
+        return d
+
+    def on_chat_poll_start(self, player_id):
+        """
+        Called when a new chat poll is initiated by a player.
+        Mark the player as being online.
+        """
+
+        if player_id not in self.online_players:
+            self.online_players[player_id] = { 'active_polls': 1 }
+            self.notify({'type': 'player_connecting',
+                         'player_id': player_id})
+        else:
+            self.online_players[player_id]['active_polls'] += 1
+
+
+    def on_chat_poll_end(self, player_id):
+        """
+        Called when a chat poll is dropped. 
+        Mark the player as being offline if he doesn't have another active poll 
+        and doesn't start a new one quickly after (need to give time to reconnect)
+        """
+
+        self.online_players[player_id]['active_polls'] -= 1
+
+        def on_poll_resume_timeout():
+            if player_id not in self.online_players or self.online_players[player_id]['active_polls'] <= 0:
+                del self.online_players[player_id]
+
+                self.notify({'type': 'player_disconnecting',
+                             'player_id': player_id})
+
+        if self.online_players[player_id]['active_polls'] <= 0:
+            # give X seconds to start another poll
+            reactor.callLater(15, on_poll_resume_timeout)
+
+    def state(self, args):
+        """
+        Shows the players currently online, along with the number of active chat polls
+        """
+
+        return defer.succeed([{"online_players": self.online_players}, []])
+
+    def is_player_online(self, player_id):
+        """
+        Checks if the player is currently online
+        """
+
+        return player_id in self.online_players
+
---
+++ plugins/activity/test_activity.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Farsides <contact@farsides.com>
+#
+# Authors:
+#          Xavier Antoviaque <xavier@antoviaque.org>
+#
+# This software's license gives you freedom; you can copy, convey,
+# propagate, redistribute and/or modify this program under the terms of
+# the GNU Affero General Public License (AGPL) as published by the Free
+# Software Foundation (FSF), either version 3 of the License, or (at your
+# option) any later version of the AGPL published by the FSF.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in a file in the toplevel directory called
+# "AGPLv3".  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports ####################################################################
+
+import sys, os
+sys.path.insert(0, os.path.abspath("../..")) # so that for M-x pdb works
+from mock import Mock
+
+from twisted.trial import unittest, runner, reporter
+from twisted.internet import defer
+
+from cardstories.service import CardstoriesService
+from plugins.activity import activity
+
+
+# Classes ####################################################################
+
+class FakeReactor(object):
+    """
+    Overrides the reactor behavior, to allow to control delayed callbacks
+    """
+
+    def __init__(self):
+        self.call_later_cb = None
+
+    def callLater(self, delay, cb):
+        self.call_later_callback = cb
+
+    def call_now(self):
+        self.call_later_callback()
+
+
+# Tests ######################################################################
+
+class ActivityTest(unittest.TestCase):
+
+    def setUp(self):
+        self.database = 'test.sqlite'
+        if os.path.exists(self.database):
+            os.unlink(self.database)
+
+        self.service = CardstoriesService({'db': self.database,
+                                           'plugins-confdir': 'CONFDIR',
+                                           'plugins-libdir': 'LIBDIR',
+                                           'static': 'STATIC'
+                                           })
+        self.service.startService()
+
+        # Don't actually make delayed calls, but allow tests to call them
+        self.default_reactor = activity.reactor.callLater
+        self.activity_reactor = FakeReactor()
+        activity.reactor = self.activity_reactor
+
+        # Fake a chat plugin on which the activity plugin should listen to
+        self.mock_chat_instance = Mock()
+        self.mock_chat_instance.name.return_value = 'chat'
+        self.activity_instance = activity.Plugin(self.service, [self.mock_chat_instance])
+
+        self.mock_chat_instance.listen.assert_called_once_with()
+        self.mock_chat_instance.reset_mock()
+
+    def tearDown(self):
+        activity.reactor = self.default_reactor
+
+        # kill the service we started before the test
+        return self.service.stopService()
+
+    @defer.inlineCallbacks
+    def check_online_players(self, online_players):
+        """
+        Check that the players currently online are the ones (and only the ones) in the list online_players_ids
+        """
+
+        state = yield self.activity_instance.state([])
+        self.assertEqual(state[0]['online_players'], online_players)
+
+        for player_id in online_players.keys():
+            is_online = self.activity_instance.is_player_online(player_id)
+            self.assertTrue(is_online, "Player id %d is not online" % player_id)
+
+    @defer.inlineCallbacks
+    def test01_online_offline(self):
+        player_id = 12
+
+        # No player initially
+        yield self.check_online_players({})
+
+        # Player going online
+        on_event_mock = Mock()
+        self.activity_instance.listen().addCallback(on_event_mock)
+        self.activity_instance.on_chat_notification({'type': 'poll_start', 'player_id': player_id})
+
+        on_event_mock.assert_called_once_with({'type': 'player_connecting', 'player_id': player_id})
+        on_event_mock.reset_mock()
+        self.activity_instance.listen().addCallback(on_event_mock)
+
+        yield self.check_online_players({player_id: {'active_polls': 1} })
+
+        # Player dropping poll
+        self.activity_instance.on_chat_notification({'type': 'poll_end', 'player_id': player_id})
+        self.assertEqual(on_event_mock.call_count, 0)
+        yield self.check_online_players({player_id: {'active_polls': 0} })
+
+        # Player starting another poll quickly enough
+        self.activity_instance.on_chat_notification({'type': 'poll_start', 'player_id': player_id})
+        self.assertEqual(on_event_mock.call_count, 0)
+        yield self.check_online_players({player_id: {'active_polls': 1} })
+
+        # Player dropping poll again, this time for good (going offline)
+        self.activity_instance.on_chat_notification({'type': 'poll_end', 'player_id': player_id})
+        self.assertEqual(on_event_mock.call_count, 0)
+        self.activity_reactor.call_now()
+
+        on_event_mock.assert_called_once_with({'type': 'player_disconnecting', 'player_id': player_id})
+        yield self.check_online_players({})
+
+
+def Run():
+    loader = runner.TestLoader()
+    suite = loader.suiteFactory()
+    suite.addTest(loader.loadClass(ActivityTest))
+
+    return runner.TrialRunner(
+        reporter.VerboseTextReporter,
+        tracebackFormat='default',
+        ).run(suite)
+
+if __name__ == '__main__':
+    if Run().wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
