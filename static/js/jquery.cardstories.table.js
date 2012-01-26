1cfba70b3adb58524d7805ffbe82c4e95c251b4f
Xavier Antoviaque <xavier@antoviaque.org>
2012-01-26 16:20:51 +0100

    Closes #760 - JS client table plugin
    Handles client-side interaction with the WS for tables. It takes care of
    setting up a poll on the WS through the plugin interface, for the
    current game, and handles the state() updates.
    
    Upon initialisation, if no game is currently selected and if game
    creation isn't explicitely requested through the ?create=1 argument, it
    requests an available table/game from the WS and redirects the browser
    to it if any. (Otherwise, it simply let the client proceed to the
    default action, which is to create a new game).
    
    Since the table plugin keeps the plugin up to date on the table
    information, the data received through the state() method is stored on
    the root, to allow direct access when needed.
    
    The plugin also exposes methods to allow to redirect to the next game
    when needed ('load_next_game_when_ready()'), which either redirects
    directly, or waits for the information about the next game to arrive if
    it isn't there yet.

---
+++ static/js/jquery.cardstories.table.js
@@ -0,0 +1,169 @@
+//
+//  Copyright (C) 2012 Farsides <contact@farsides.com>
+//
+//  Authors:
+//          Xavier Antoviaque <xavier@antoviaque.org>
+//
+//  This software's license gives you freedom; you can copy, convey,
+//  propagate, redistribute and/or modify this program under the terms of
+//  the GNU Affero General Public License (AGPL) as published by the Free
+//  Software Foundation (FSF), either version 3 of the License, or (at your
+//  option) any later version of the AGPL published by the FSF.
+//
+//  This program is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+//  General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program in a file in the toplevel directory called
+//  "AGPLv3".  If not, see <http://www.gnu.org/licenses/>.
+//
+
+(function($) {
+
+    $.cardstories_table = {
+
+        name: 'table',
+
+        poll: 'table',
+
+        init: function(player_id, game_id, root) {
+            var $this = this;
+            
+            // Table data storage
+            $this.init_game2table(game_id, root);
+            
+            // If game creation or a specific game aren't explicitly
+            // requested, try to join an active table/game
+            if(player_id && !$.query.get('create') && !game_id) {
+                var table = $this.get_table_from_game_id(game_id, root);
+                
+                table.ready_for_next_game = true;
+                $this.force_state_update(player_id, game_id, root);
+            }
+        },
+
+        // Keep tables referenced by game_id on the root, to allow to store
+        // table state between state() poll updates
+        init_game2table: function(game_id, root) {
+            // Keep a unique reference for an undefined game_id,
+            // to allow to include it in the game2table array
+            if(!game_id) {
+                game_id = 0;
+            }
+            
+            // Keep track of games table information
+            var game2table = {};
+            game2table[game_id] = { next_game_id: null,
+                                    next_owner_id: null,
+                                    ready_for_next_game: false };
+            $(root).data('cardstories_table', { 'game2table': game2table });
+        },
+        
+        // Retreive table data
+        get_table_from_game_id: function(game_id, root) {
+            var game2table = $(root).data('cardstories_table').game2table;
+
+            if(!game_id) {
+                game_id = 0;
+            }
+            var table = game2table[game_id];
+            
+            return table;
+        },
+
+        // Normally state() is called when a poll returns, but this allows
+        // to get the state updated immediately (useful when polls aren't running)
+        force_state_update: function(player_id, game_id, root) {
+            var $this = this;
+            
+            var query = {
+                action: 'state',
+                type: 'table',
+                modified: 0,
+                game_id: game_id,
+                player_id: player_id,
+            }
+            
+            var success = function(data, status) {
+                if ('error' in data) {
+                    $.cardstories.panic(data.error);
+                } else {
+                    $this.state(player_id, data, root);
+                }
+            };
+            
+            $.cardstories.ajax({
+                url: $.cardstories.url + '?' + $.param(query, true),
+                success: success
+            });
+        },
+
+        // Receives state data from the server
+        // Store it for later use or proceed to next game if player is waiting for it
+        state: function(player_id, data, root) {
+            var $this = this;
+            var game_id = data.game_id;
+            var table = $this.get_table_from_game_id(game_id, root);
+            
+            table.next_game_id = data.next_game_id;
+            table.next_owner_id = data.next_owner_id;
+            
+            $this.check_next_game(player_id, game_id, root);
+        },
+
+        // Check if we are ready to redirect to the next game
+        // (and do it if we are)
+        check_next_game: function(player_id, game_id, root) {
+            var $this = this;
+            var table = $this.get_table_from_game_id(game_id, root);
+            
+            if(table.ready_for_next_game) {
+                // It's the player's turn to create a game
+                if(player_id === table.next_owner_id) {
+                    var options = { 'force_create': true };
+                    if(game_id) {
+                        options.previous_game_id = game_id;
+                    }
+                    $.cardstories.reload(undefined, options);
+                    return true;
+                    
+                // Next game is ready to be joined
+                } else if(table.next_game_id && table.next_game_id !== game_id) {
+                    $.cardstories.reload(table.next_game_id);
+                    return true;
+                }
+            }
+            return false;
+        },
+        
+        // Reload to the next game as soon it is created
+        load_next_game_when_ready: function(player_id, game_id, root) {
+            var $this = this;
+            var table = $this.get_table_from_game_id(game_id, root);
+            
+            table.ready_for_next_game = true;
+            var is_ready = $this.check_next_game(player_id, game_id, root);
+            
+            return is_ready;
+        },
+        
+        // Returns the player_id of the player who should create the next game
+        get_next_owner_id: function(player_id, game_id, root) {
+            var $this = this;
+            var table = $this.get_table_from_game_id(game_id, root);
+ 
+            return table.next_owner_id;
+        }
+    };
+
+    $.fn.cardstories_table = function(player_id, game_id) {
+        $.cardstories_table.init(player_id, game_id, this);
+        return this;
+    };
+
+    // Register cardstories plugin
+    $.cardstories.register_plugin($.cardstories_table);
+
+})(jQuery);
---
+++ static/test/table.js
@@ -0,0 +1,156 @@
+var selector = '#cardstories_root';
+var default_query_get = $.query.get;
+var default_cardstories_ajax = $.cardstories.ajax;
+var default_cardstories_reload = $.cardstories.reload;
+var default_cardstories_table_force_state_update = $.cardstories_table.force_state_update;
+
+function setup() {
+    var root = $(selector);
+    
+    $.query.get = function() { throw 'Please rebind "$.query.get"'; };
+    $.cardstories.ajax = function() { throw 'Please rebind "ajax"'; };
+    $.cardstories.reload = function() { throw 'Please rebind "reload"'; };
+    
+    $.cardstories_table.force_state_update = default_cardstories_table_force_state_update; 
+}
+
+module("cardstories_table", {setup: setup});
+
+test("init_create", 4, function() {
+    var root = $(selector);
+    var player_id = 1;
+    
+    $.query.get = function(param_name) {
+        if(param_name == 'create') { return true; };
+    };
+    $.cardstories_table.force_state_update = function(_player_id, _game_id, _root) { 
+        ok(false, "Plugin shouldn't intervene when a game creation is explicitly requested");     
+    };
+    
+    root.cardstories_table(player_id, undefined);
+    equal($.cardstories.get_hash_length($(root).data('cardstories_table').game2table), 1);
+    equal($(root).data('cardstories_table').game2table[0].next_game_id, null);
+    equal($(root).data('cardstories_table').game2table[0].next_owner_id, null);
+    equal($(root).data('cardstories_table').game2table[0].ready_for_next_game, false);
+});
+
+test("init_game_id", 4, function() {
+    var root = $(selector);
+    var player_id = 1;
+    var game_id = 30;
+    
+    $.query.get = function(param_name) {
+        if(param_name == 'create') { return false; };
+    };
+    $.cardstories_table.force_state_update = function(_player_id, _game_id, _root) { 
+        ok(false, "Plugin shouldn't intervene when a game_id is explicitly requested");     
+    };
+    
+    root.cardstories_table(player_id, game_id);
+    equal($.cardstories.get_hash_length($(root).data('cardstories_table').game2table), 1);
+    equal($(root).data('cardstories_table').game2table[game_id].next_game_id, null);
+    equal($(root).data('cardstories_table').game2table[game_id].next_owner_id, null);
+    equal($(root).data('cardstories_table').game2table[game_id].ready_for_next_game, false);
+});
+
+test("init_find_game", 4, function() {
+    var root = $(selector);
+    var player_id = 1;
+    var game_id = undefined;
+    
+    $.query.get = function(param_name) {
+        if(param_name == 'create') { return false; };
+    };
+    $.cardstories_table.force_state_update = function(_player_id, _game_id, _root) {
+        // Should request a game to join when no game specified nor create
+        equal(player_id, _player_id);
+        equal(game_id, _game_id);
+    };
+    
+    root.cardstories_table(player_id, game_id);
+    equal($.cardstories.get_hash_length($(root).data('cardstories_table').game2table), 1);
+    equal($(root).data('cardstories_table').game2table[0].ready_for_next_game, true);
+});
+
+// Init the table plugin without triggering reload 
+init_table = function(player_id, game_id, root) {
+    $.query.get = function(param_name) {
+        if(param_name == 'create') { return false; };
+    };
+    root.cardstories_table(player_id, game_id);
+};
+
+test("force_state_update", 1, function() {
+    var root = $(selector);
+    var player_id = 1;
+    var game_id = 15;
+    init_table(player_id, game_id, root);
+    
+    $.cardstories.ajax = function(options) {
+        equal(options.url, '../resource?action=state&type=table&modified=0&game_id='+game_id+'&player_id='+player_id);
+    };
+
+    $.cardstories_table.force_state_update(player_id, game_id, root);
+});
+
+test("state_next_as_author", 8, function() {
+    var root = $(selector);
+    var player_id = 1;
+    var game_id = 15;
+    init_table(player_id, game_id, root);
+    
+    // First get poll data
+    var data = {type: 'table', 
+                player_id: player_id, 
+                game_id: game_id,
+                next_game_id: null,
+                next_owner_id: player_id};
+    
+    $.cardstories_table.state(player_id, data, root);
+    equal($(root).data('cardstories_table').game2table[game_id].next_game_id, null);
+    equal($(root).data('cardstories_table').game2table[game_id].next_owner_id, player_id);
+    equal($(root).data('cardstories_table').game2table[game_id].ready_for_next_game, false);
+    
+    // Then reload when ready
+    $.cardstories.reload = function(_game_id, options) {
+        equal(_game_id, undefined);
+        equal(options.previous_game_id, game_id);
+        equal(options.force_create, true);
+    };
+    
+    $.cardstories_table.load_next_game_when_ready(player_id, game_id, root);
+    equal($(root).data('cardstories_table').game2table[game_id].ready_for_next_game, true);
+    
+    equal($.cardstories_table.get_next_owner_id(player_id, game_id, root), player_id);
+});
+
+test("state_next_as_player", 6, function() {
+    var root = $(selector);
+    var player1 = 10;
+    var player2 = 12
+    var game1 = 15;
+    var game2 = 17;
+    init_table(player1, game1, root);
+    
+    // Ready before receiving next game data - wait
+    equal($(root).data('cardstories_table').game2table[game1].ready_for_next_game, false);
+    $.cardstories_table.load_next_game_when_ready(player1, game1, root);
+    equal($(root).data('cardstories_table').game2table[game1].ready_for_next_game, true);
+    
+    // Reload as soon as next game_id is received in state info
+    $.cardstories.reload = function(next_game_id, options) {
+        equal(next_game_id, game2);
+    };
+    
+    var data = {type: 'table', 
+                player_id: player1, 
+                game_id: game1,
+                next_game_id: game2,
+                next_owner_id: player2};
+    $.cardstories_table.state(player1, data, root);
+    equal($(root).data('cardstories_table').game2table[game1].next_game_id, game2);
+    equal($(root).data('cardstories_table').game2table[game1].next_owner_id, player2);
+    
+    equal($.cardstories_table.get_next_owner_id(player1, game1, root), player2);
+});
+
